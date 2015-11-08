7e1679feb75a2c804895cb992779d4cadd1d0597
Hemstreet <jonhemstreet@gmail.com>
2015-11-07 19:52:56 -0700

    Added base model schema support

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose'),
+    baseSchema = require('../schemas/base.json'),
     userSchema = require('../schemas/user.json'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    _ = require('lodash');
 
-module.exports = mongoose.model('User', new Schema(userSchema));
+module.exports = mongoose.model('User', new Schema(_.extend(baseSchema, userSchema)));
