91ce6258ad2c89d912f3766829ea52de50cde8f1
Hemstreet <jonhemstreet@gmail.com>
2015-11-14 14:26:51 -0700

    Node uncapitalize middleware

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ model.getModels().then(function (models) {
     app.use(bodyParser.json());
     app.use(cookieParser());
     app.use(morgan(env));
+    app.use(require('express-uncapitalize')());
 
     app.use('/v1/rest', router);
     app.use('/', express.static(__dirname + '/app'));
