fd79e7cc243f0beb40cbb1fce0cb32e87f61bec9
Hemstreet <jonhemstreet@gmail.com>
2016-03-27 18:28:46 -0600

    Updated to allow CORS Updated dashboard Removed hardcoded ballmer path

--- ballmer/rest.js
+++ ballmer/rest.js
@@ -1,4 +1,4 @@
 var Server = require('../server.js'),
     server = new Server({
-        modelPath: "/Users/jonhemstreet/Development/Sandbox/MEAN-Street/ballmer/models/"
+        modelPath: __dirname + "/models/"
     });
