c99f1b4727060d955b9cc85b11ba63b8d4a8e563
Hemstreet <jonhemstreet@gmail.com>
2016-07-20 18:28:38 -0600

    Added gulp parameter passing

--- tasks/run.js
+++ tasks/run.js
@@ -1,13 +1,25 @@
 var nodemon = require('nodemon');
+var argv = require('yargs').argv;
+
 
 module.exports = function (gulp, plugins, config) {
+
+    function buildArgList() {
+        var args = [];
+        config.flags.forEach(function(key) {
+            if (argv[key]) {
+                args.push("--" + key + "=" + argv[key]);
+            }
+        });
+
+        return args;
+    };
+
     gulp.task('run', function () {
         nodemon({
             script: './example/server.js',
             ext: 'js html json',
-            args: config.flags.map(function (key) {
-                return '--' + key;
-            })
+            args: buildArgList()
         })
     });
 }
